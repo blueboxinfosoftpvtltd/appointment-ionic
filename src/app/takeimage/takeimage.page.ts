35bae067e147f7d365affa122d167b72d292928f
Bhagirath Dangar <bhagirathdangar@blueboxinfosoft.com>
2020-11-25 15:40:55 +0530

    minor change, dummy code commented

--- src/app/takeimage/takeimage.page.ts
+++ src/app/takeimage/takeimage.page.ts
@@ -1095,7 +1095,7 @@ export class TakeimagePage implements OnInit {
       (err: CaptureError) => {
         console.log("CAPTURE_ERROR", err);
 
-        this.dummyUpload();
+        // this.dummyUpload();
 
         this.authservice.dismissLoading();
       }
